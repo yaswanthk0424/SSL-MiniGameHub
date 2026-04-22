2902de462d3808fea3782e2f2c95e619c8cf222a
yaswanthk0424 <explorer.0424@gmail.com>
2026-04-22 23:55:45 +0530

    Updated leaderboard.sh

--- hub/leaderboard.sh
+++ hub/leaderboard.sh
@@ -1,87 +1,63 @@
 #!/bin/bash
-echo -e "===========Game1 Leaderboard=========="
-printf "\n%-12s %-10s %-10s %-10s %-10s %-10s\n" "Player_name" "Played" "Wins" "Losses" "Win pct" "Loss pct"
-sed -nE '/^[0-9]{2}-[0-9]{2}-[0-9]{4},Game1,[A-Za-z]+,[A-Za-z]+$/p' history.csv > game1_lb.csv
-awk -F',' '
-{
 
-    winner=$3;
-    loser=$4;
-    played[winner]++;
-    played[loser]++;
-    wins[winner]++;
-    losses[loser]++;
-    losses[winner] = played[winner] - wins[winner];
-    wins[loser] = played[loser] - losses[loser];
+print_leaderboard() {
 
-}
-END {
-for(name in played){ 
-    win_pct = wins[name]*100/played[name];
-    loss_pct = losses[name]*100/played[name];
-    printf "%s\t %d\t %d\t %d\t %d\t %d\n",name,played[name],wins[name],losses[name],win_pct,loss_pct >> "dummy_output.txt"
-}
+    game_name=$1
+    echo ""
+    echo "=========== ${game_name} Leaderboard ==========="
 
-}
-' game1_lb.csv
-sort -n -t$'\t' -k4 dummy_output.txt | awk -F'\t' '{printf"%-12s %-10d %-10d %-10d %-10.2f %-10.2f\n",$1,$2,$3,$4,$5,$6}'
-rm dummy_output.txt
-rm game1_lb.csv
+    printf "%-12s %-10s %-10s %-10s %-10s %-10s\n" \
+    "Player_name" "Played" "Wins" "Losses" "Win_pct" "Loss_pct"
 
-echo -e "\n===========Game2 Leaderboard=========="
-printf "\n%-12s %-10s %-10s %-10s %-10s %-10s\n" "Player_name" "Played" "Wins" "Losses" "Win pct" "Loss pct"
-sed -nE '/^[0-9]{2}-[0-9]{2}-[0-9]{4},Game2,[A-Za-z]+,[A-Za-z]+$/p' history.csv > game2_lb.csv
-awk -F',' '
-{
+    awk -F',' -v game="$game_name" '
+    {
+        if (NR > 1) {
+            if ($1 == game) {
+                if ($5 != "Yes") {
+                    winner = $3
+                    loser  = $4
 
-    winner=$3;
-    loser=$4;
-    played[winner]++;
-    played[loser]++;
-    wins[winner]++;
-    losses[loser]++;
-    losses[winner] = played[winner] - wins[winner];
-    wins[loser] = played[loser] - losses[loser];
+                    played[winner]++
+                    played[loser]++
 
-}
-END {
-for(name in played){ 
-    win_pct = wins[name]*100/played[name];
-    loss_pct = losses[name]*100/played[name];
-    printf "%s\t %d\t %d\t %d\t %d\t %d\n",name,played[name],wins[name],losses[name],win_pct,loss_pct >> "dummy2_output.txt"
-}
+                    wins[winner]++
+                    losses[loser]++
+                }
+            }
+        }
+    }
 
-}
-' game2_lb.csv
-sort -n -t$'\t' -k4 dummy2_output.txt | awk -F'\t' '{printf"%-12s %-10d %-10d %-10d %-10.2f %-10.2f\n",$1,$2,$3,$4,$5,$6}'
-rm dummy2_output.txt
-rm game2_lb.csv
+    END {
 
-echo -e "\n===========Game3 Leaderboard=========="
-printf "\n%-12s %-10s %-10s %-10s %-10s %-10s\n" "Player_name" "Played" "Wins" "Losses" "Win pct" "Loss pct"
-sed -nE '/^[0-9]{2}-[0-9]{2}-[0-9]{4},Game3,[A-Za-z]+,[A-Za-z]+$/p' history.csv > game3_lb.csv
-awk -F',' '
-{
+        for (name in played) {
 
-    winner=$3;
-    loser=$4;
-    played[winner]++;
-    played[loser]++;
-    wins[winner]++;
-    losses[loser]++;
-    losses[winner] = played[winner] - wins[winner];
-    wins[loser] = played[loser] - losses[loser];
+            w = wins[name]
+            l = losses[name]
+            p = played[name]
 
-}
-END {
-for(name in played){ 
-    win_pct = wins[name]*100/played[name];
-    loss_pct = losses[name]*100/played[name];
-    printf "%s\t %d\t %d\t %d\t %d\t %d\n",name,played[name],wins[name],losses[name],win_pct,loss_pct >> "dummy3_output.txt"
+            if (w == "") w = 0
+            if (l == "") l = 0
+
+            if (p > 0) {
+                win_pct  = (w * 100) / p
+                loss_pct = (l * 100) / p
+            } else {
+                win_pct  = 0
+                loss_pct = 0
+            }
+
+            printf "%s\t%d\t%d\t%d\t%.2f\t%.2f\n", \
+            name, p, w, l, win_pct, loss_pct
+        }
+    }' history.csv |
+    sort -t$'\t' -k5 -nr |
+    awk -F'\t' '{
+        printf "%-12s %-10d %-10d %-10d %-10.2f %-10.2f\n",
+        $1,$2,$3,$4,$5,$6
+    }'
 }
 
-}
-' game3_lb.csv
-sort -n -t$'\t' -k4 dummy3_output.txt | awk -F'\t' '{printf"%-12s %-10d %-10d %-10d %-10.2f %-10.2f\n",$1,$2,$3,$4,$5,$6}'
-rm dummy3_output.txt
-rm game3_lb.csv
+# RUN
+print_leaderboard "TicTacToe"
+print_leaderboard "Connect4"
+print_leaderboard "Othello"
